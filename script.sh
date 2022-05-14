c4013a2b4b262f821cb6d10f67ca27ac98adf61a
Aibek_kz <pigeonthe@gmail.com>
2022-05-14 23:55:48 +0600

    Delited all files

--- script.sh
+++
@@ -1,8 +0,0 @@
-echo "Building image"
-docker build -t forum .
-echo "Running image"
-docker run -d -p 8080:8080 --name forum forum
-echo "Images list"
-docker images
-echo "Containers list"
-docker container ls
